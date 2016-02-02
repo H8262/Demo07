9f4857c200208b4490cb9530dadd24c0acdefef3
Timo Heinonen <h8262@student.jamk.fi>
2016-02-02 17:52:12 +0200

    5/5

---
+++ Demo07/T3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TV program = new TV();
+
+            TV program1 = new TV { Name = "Teletapit", Channel = "Kakkonen", Start = "3:00", End = "20:00", Description = "Uudestaan" };
+            TV program2 = new TV { Name = "Salapoliisiohjelma", Channel = "Perkele", Start = "1:00", End = "1:15", Description = "Paras ikinä" };
+            TV program3 = new TV { Name = "Tosi-Tv", Channel = "Muijienitkukanava", Start = "6:66", End = "Ajan rakenne muuttunut", Description = "Ihana David" };
+
+            program.AddTv(program1);
+            program.AddTv(program2);
+            program.AddTv(program3);
+
+            System.IO.StreamWriter tvPrograms = new System.IO.StreamWriter("programs.txt");
+           
+            tvPrograms.WriteLine(program1.ToString());
+            tvPrograms.WriteLine(program2.ToString());
+            tvPrograms.WriteLine(program3.ToString());
+            tvPrograms.Close();
+            string text = System.IO.File.ReadAllText("programs.txt");
+            System.Console.WriteLine("Programs: " + text);          
+        }
+    }
+}
---
+++ Demo07/T3/TV.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class TV
+    {
+        public string Name { get; set; }
+        public string Channel { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Description { get; set; }
+
+        private List<TV> programs;
+        public TV()
+        {
+            programs = new List<TV>();
+        }
+
+        public void AddTv(TV tv)
+        {
+            programs.Add(tv);
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + " Channel: " + Channel + " Start time_ " + Start + " End " + End + " Description: " + Description;
+        }
+        
+    }
+}

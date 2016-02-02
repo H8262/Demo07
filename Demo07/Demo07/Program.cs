71af618b457668c01c6d88784e3b3eb6065c071d
H8262 <h8262@student.jamk.fi>
2016-02-02 11:09:45 +0200

    tsajajaja

---
+++ Demo07/Demo07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter("test.txt");
+            outputFile.WriteLine(Console.ReadLine());
+            outputFile.Close();
+            string text = System.IO.File.ReadAllText("test.txt");
+            System.Console.WriteLine("File contains: " + text);
+        }
+    }
+}
---
+++ Demo07/T2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamWriter integers = new System.IO.StreamWriter("integers.txt");
+            System.IO.StreamWriter doubles = new System.IO.StreamWriter("doubles.txt");
+            string text = null;
+            int number;
+            double number1;
+            
+            do
+            {
+                Console.WriteLine("Give number: ");             
+                try
+                {
+                    text = Console.ReadLine();
+                    bool integer = int.TryParse(text, out number);
+                    bool tuplat = double.TryParse(text, out number1);
+
+                    if (integer)
+                    {
+                        integers.WriteLine(text);
+                    }
+                    else if (tuplat)
+                    {
+                        doubles.WriteLine(text);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                }
+            } while (text.Length != 0);
+            integers.Close();
+            doubles.Close();
+
+            string values = System.IO.File.ReadAllText("integers.txt");
+            string values1 = System.IO.File.ReadAllText("doubles.txt");
+
+            Console.WriteLine("Integers: " + values);
+            Console.WriteLine("Doubles: " + values1);       
+        }
+    }
+}
